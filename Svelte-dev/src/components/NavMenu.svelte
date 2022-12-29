ca47d69499b926baf6c57c3c3f11aa39e6c73edf
ActionHOSchT <raffael-georg.krakau@informatik.hs-fulda.de>
2022-12-29 12:09:13 +0100

    unused css commented

--- Svelte-dev/src/components/NavMenu.svelte
+++ Svelte-dev/src/components/NavMenu.svelte
@@ -10,10 +10,12 @@
 </main>
 
 <style>
+    /*
     .navMain {
         background-color: #333333;
         color: #18b218;
         border: #000;
         border-radius: 0.5em;
     }
+    */
 </style>
